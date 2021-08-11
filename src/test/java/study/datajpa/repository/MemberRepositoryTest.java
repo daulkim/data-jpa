1c42d6d116ac868bf73f85c4d65a8d7df1adf4a4
daulkim <kimdu0825@gmail.com>
2021-08-12 00:31:54 +0900

    네이티브 쿼리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -412,4 +412,29 @@ class MemberRepositoryTest {
 
         }
     }
+
+    @Test
+    public void nativeQuery() {
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        Page<MemberProjection> result = memberRepository.findbByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("username = " + memberProjection.getUsername());
+            System.out.println("teamName = " + memberProjection.getTeamName());
+        }
+    }
+
+
 }
